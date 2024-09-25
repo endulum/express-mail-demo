1b664984b520493b318817c524f16dec4f7fb559
endulum <71853916+endulum@users.noreply.github.com>
2024-09-25 14:08:36 -0700

    have ejs render html in mail

--- src/routes/emailRouter.ts
+++ src/routes/emailRouter.ts
@@ -3,6 +3,9 @@ import asyncHandler from 'express-async-handler'
 import handleValidationErrors from '../middleware/handleValidationErrors'
 import { RequestHandler } from 'express-serve-static-core'
 import { body, ValidationChain } from 'express-validator'
+import ejs from 'ejs'
+import path from 'path'
+
 import transporter from '../nodemailer'
 
 const router = express.Router()
@@ -26,6 +29,9 @@ const email: {
       .trim()
       .notEmpty().withMessage('Please input an email.')
       .isEmail().withMessage('Not a valid email.')
+      .escape(),
+    body('message')
+      .trim()
       .escape()
   ],
 
@@ -35,14 +41,23 @@ const email: {
     if (!process.env.SMTP_EMAIL)
       throw new Error('Sendee is not defined.');
 
-    const info = await transporter.sendMail({
-      from: process.env.SMTP_EMAIL,
-      to: req.body.email,
-      subject: 'Does this work?',
-      text: 'Guess it does!'
-    })
+    try {
+      const htmlPath = path.join(__dirname, '..', '..', '/src/views/mail.ejs')
+      const html = await ejs.renderFile(htmlPath, {
+        message: req.body.message
+      });
 
-    console.log(info)
+      const info = await transporter.sendMail({
+        from: process.env.SMTP_EMAIL,
+        to: req.body.email,
+        subject: 'Does this work?',
+        html
+      })
+    } catch(err) {
+      console.error(err);
+      return email.render(req, res, next);
+    }
+
     // end email
     return res.redirect('/success')
   })
